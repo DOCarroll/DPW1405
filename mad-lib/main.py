579e1b91925a0d699e3eeb4f9f9444458c780548
Danny O'Carroll <LemonMyth@live.com>
2014-05-05 22:41:55 -0400

    Added an Array

--- mad-lib/main.py
+++ mad-lib/main.py
@@ -3,13 +3,16 @@ name: Daniel O'Carroll
 date: 5/4/14
 class: DPW1405
 '''
-
+#String User Inputs
 first_name = raw_input('Enter your first name: ')
 last_name = raw_input('Enter your last name: ')
 gender = raw_input('Enter your gender (M or F): ')
 
+#Number User Inputs
 age = input('Enter your age: ')
 brothers = input('How many brothers do you have?(Enter a number): ')
 full_sail = input('How many years have you been at Full Sail?(Enter a number): ')
 
+#Array
 monsters = ['Troll', 'Ogre', 'Monstrous Donkeys']
+
