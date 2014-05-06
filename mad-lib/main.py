977a5e3ff906c9e3a126b29089b66c0a5bdbfe48
Danny O'Carroll <LemonMyth@live.com>
2014-05-05 22:40:01 -0400

    Added user input variables for numbers

--- mad-lib/main.py
+++ mad-lib/main.py
@@ -7,3 +7,7 @@ class: DPW1405
 first_name = raw_input('Enter your first name: ')
 last_name = raw_input('Enter your last name: ')
 gender = raw_input('Enter your gender (M or F): ')
+
+age = input('Enter your age: ')
+brothers = input('How many brothers do you have?(Enter a number): ')
+full_sail = input('How many years have you been at Full Sail?(Enter a number): ')
