f1335decc1992795b1120d32c002d19546e82449
Danny O'Carroll <LemonMyth@live.com>
2014-05-05 23:50:08 -0400

    Got all functions and prints working correctly.

--- mad-lib/main.py
+++ mad-lib/main.py
@@ -16,13 +16,13 @@ full_sail = input('How many years have you been at Full Sail?(Enter a number): '
 
 #Array
 monsters = ['Troll', 'an Ogre', 'and a Monstrous Donkey']
+john_dave = ['John', 'and Dave']
 
 #Dictionary
-new_brothers = {'new': 'John', 'old': monsters}
+new_brothers = {'new': john_dave, 'old': monsters}
 
 #Mathematical Operators
 full_sail = full_sail + 2000
-computer_graphics = full_sail - 1200
 full_name = first_name + ' ' + last_name
 
 def print_monsters(b):
@@ -41,23 +41,26 @@ if age < 20 or age > 25:
     You spent most of your life at Full Sail, you are now {age}!
     """
     part1 = part1.format(**locals())
-else:
+elif age > 20 or age < 25:
     part1 = """
     Hello, {suffix} {full_name},
-    You picked a good time to come to Full Sail!
+    You picked a good time to come to Full Sail, you're so young!
     """
+    part1 = part1.format(**locals())
 if brothers < 3:
     brothers = new_brothers['new']
     part2 = """
-    All the brothers you grew up with died, now you are left with your new brother {brothers}.
+    All the brothers you grew up with died, now you are left with your new brothers
     """
     part2 = part2.format(**locals())
-    print_monsters(brothers)
+
 else:
     brothers = new_brothers['old']
     part2 = """
     Some of your brothers died, or were turned into monsters. Now your brothers are a
     """
+
+
 
 def story(age_part, brothers_part):
     print age_part
@@ -66,5 +69,5 @@ def story(age_part, brothers_part):
 
 
 story(part1, part2)
-
+print_monsters(brothers)
 
