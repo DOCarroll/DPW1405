ac489344afbad5eb78bd50c6260ccc5e486e6d0a
Danny O'Carroll <LemonMyth@live.com>
2014-05-05 22:36:27 -0400

    Added user input variables

---
+++ mad-lib/main.py
@@ -0,0 +1,9 @@
+'''
+name: Daniel O'Carroll
+date: 5/4/14
+class: DPW1405
+'''
+
+first_name = raw_input('Enter your first name: ')
+last_name = raw_input('Enter your last name: ')
+gender = raw_input('Enter your gender (M or F): ')
