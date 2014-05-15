7dd3489eac8e114235ae71c47c0bce41c6b60ef9
Danny O'Carroll <LemonMyth@live.com>
2014-05-14 22:32:42 -0400

    Created Page class, added some functionality

--- fox/main.py
+++ fox/main.py
@@ -8,19 +8,35 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+#Create a Page class
+class Page(object):
+    def __init__(self):
+        self._open = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+<body>'''
+        self._content = '''
+This is where all of the content will go
+        '''
+
+
+
 #Create the Abstract Animal Class
 class AbstractAnimal(object):
     def __init__(self):
         #define properties of animals
-        self._phylum = ''
-        self._class = ''
-        self._order = ''
-        self._family = ''
-        self._genus = ''
-        self._image = ''
-        self._life_span = ''
-        self._habitat = ''
-        self._geo_location = ''
+        self._phylum = 'Test'
+        self._class = 'Test'
+        self._order = 'Test'
+        self._family = 'Test'
+        self._genus = 'Test'
+        self._image = 'Test'
+        self._life_span = 'Test'
+        self._habitat = 'Test'
+        self._geo_location = 'Test'
 
         animal = [self._phylum, self._class, self._order, self._family, self._genus, self._image, self._life_span, self._habitat, self._geo_location]
         print animal
