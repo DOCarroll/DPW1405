bc40c291bb5e82e3467d39e60a127bd765aaacff
Danny O'Carroll <LemonMyth@live.com>
2014-05-19 17:37:53 -0400

    Create Page Class

--- foxSay/main.py
+++ foxSay/main.py
@@ -8,61 +8,92 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+        self.response.write(p.print_page())
 
 
+#Create Page Class
+class Page(object):
+    def __init__(self):
+        self.open = '''
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+    </head>
+<body>'''
+        self.close = '''
+</body>
+</html>'''
+        self.content = '''
+<h3></h3>
+<p>Phylum: </p>
+<p>Class: </p>
+<p>Order: </p>
+<p>Family: </p>
+<p>Genus: </p>
+<p>Img: </p>
+<p>Life Span: </p>
+<p>Habitat: </p>
+<p>Geo Location: </p>'''
+
+    def print_page(self):
+        return self.open + self.content + self.close
 
 #Create Animal Class
 class AbstractAnimal(object):
     def __init__(self):
-        self._phylum = ''
-        self._class = ''
-        self._order = ''
-        self._family = ''
-        self._genus = ''
-        self._image = ''
-        self._life = ''
-        self._habitat = ''
-        self._location = ''
+        self.name = ''
+        self.phylum = ''
+        self.animal_class = ''
+        self.order = ''
+        self.family = ''
+        self.genus = ''
+        self.image = ''
+        self.life = ''
+        self.habitat = ''
+        self.location = ''
 #Create First Animal
 RedFox = AbstractAnimal()
-RedFox._phylum = 'BS'
-RedFox._class = 'BS'
-RedFox._order = 'BS'
-RedFox._family = 'BS'
-RedFox._genus = 'BS'
-RedFox._image = 'BS'
-RedFox._life = 'BS'
-RedFox._habitat = 'BS'
-RedFox._location = 'BS'
+RedFox.name = 'Red Fox'
+RedFox.phylum = 'BS'
+RedFox.animal_class = 'BS'
+RedFox.order = 'BS'
+RedFox.family = 'BS'
+RedFox.genus = 'BS'
+RedFox.image = 'BS'
+RedFox.life = 'BS'
+RedFox.habitat = 'BS'
+RedFox.location = 'BS'
 
 #Create Second Animal
 ArcticWolf = AbstractAnimal()
-ArcticWolf._phylum = 'BS'
-ArcticWolf._class = 'BS'
-ArcticWolf._order = 'BS'
-ArcticWolf._family = 'BS'
-ArcticWolf._genus = 'BS'
-ArcticWolf._image = 'BS'
-ArcticWolf._life = 'BS'
-ArcticWolf._habitat = 'BS'
-ArcticWolf._location = 'BS'
+ArcticWolf.name = 'Arctic Wolf'
+ArcticWolf.phylum = 'BS'
+ArcticWolf.animal_class = 'BS'
+ArcticWolf.order = 'BS'
+ArcticWolf.family = 'BS'
+ArcticWolf.genus = 'BS'
+ArcticWolf.image = 'BS'
+ArcticWolf.life = 'BS'
+ArcticWolf.habitat = 'BS'
+ArcticWolf.location = 'BS'
 
 #Create Third Animal
 PolarBear = AbstractAnimal()
-PolarBear._phylum = 'BS'
-PolarBear._class = 'BS'
-PolarBear._order = 'BS'
-PolarBear._family = 'BS'
-PolarBear._genus = 'BS'
-PolarBear._image = 'BS'
-PolarBear._life = 'BS'
-PolarBear._habitat = 'BS'
-PolarBear._location = 'BS'
+PolarBear.name = 'Polar Bear'
+PolarBear.phylum = 'BS'
+PolarBear.animal_class = 'BS'
+PolarBear.order = 'BS'
+PolarBear.family = 'BS'
+PolarBear.genus = 'BS'
+PolarBear.image = 'BS'
+PolarBear.life = 'BS'
+PolarBear.habitat = 'BS'
+PolarBear.location = 'BS'
 
 #Create Array to Store Animals
 animals = [RedFox, ArcticWolf, PolarBear]
-
 
 
 print animals
