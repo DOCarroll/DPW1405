036af9101cfdc7e9fff9a7eb1e026fc4add6003d
Danny O'Carroll <LemonMyth@live.com>
2014-05-21 23:28:33 -0400

    Create a page class

---
+++ getters-setters-quiz/main.py
@@ -0,0 +1,37 @@
+#Daniel O'Carroll
+#May 21st, 2014
+#DPW1405
+
+import webapp2
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+
+
+#create a page class
+class Page(object):
+    def __init__(self):
+        self.counter = Counter()
+        self._open = '''
+<!DOCTYPE html>
+<head>
+<title>Counter</title>
+</head>
+<body>
+        '''
+        self._content = ''
+        self._close = '''
+</body>
+</html>
+        '''
+        self.all = self._open + self._content + self._close
+
+    def print_out(self):
+        self.all = self.all.format(**locals())
+        return self.all
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
